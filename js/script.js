dbc9ca46420817593c894d561016817496477a53
KamiYumi <111126080+KamilaYumiHirata@users.noreply.github.com>
2024-08-26 11:52:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = `Em 2049, ${nome}`;
+    caixaPerguntas.textContent = `Em sua jornada artística, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
